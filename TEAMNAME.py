b3711a3596ab90f3d4fdedddad0fbf15e7fa1d0f
rajonya-eng <53978222+rajonya-eng@users.noreply.github.com>
2021-04-05 20:24:46 +0530

    Update TEAMNAME.py

--- TEAMNAME.py
+++ TEAMNAME.py
@@ -1,3 +1,9 @@
+#Сhef has assembled a football team! Now he needs to choose a name for his team. There are N words in English that Chef considers funny. These words are s1,s2,…,sN.
+
+#Chef thinks that a good team name should consist of two words such that they are not funny, but if we swap the first letters in them, the resulting words are funny. Note that under the given constraints, this means that a word is a non-empty string containing only lowercase English letters.
+
+#Can you tell Chef how many good team names there are?
+
 # cook your dish here
 
 for t in range(int(input())):
